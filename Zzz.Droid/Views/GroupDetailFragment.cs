8ddc4c34cae9eb327370153ccb961c8522c788fe
LAM <tuan.lam@marlink.com>
2017-09-10 17:57:16 +0200

    Add action bar

--- Zzz.Droid/Views/GroupDetailFragment.cs
+++ Zzz.Droid/Views/GroupDetailFragment.cs
@@ -7,6 +7,7 @@ using MvvmCross.Droid.Support.V4;
 using Zzz.Core.ViewModels;
 using Zzz.Droid.Activities;
 using Zzz.Droid.Extensions;
+using Android.Widget;
 
 namespace Zzz.Droid.Views
 {
@@ -17,9 +18,57 @@ namespace Zzz.Droid.Views
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             base.OnCreateView(inflater, container, savedInstanceState);
+            // Show the options menu.
+            HasOptionsMenu = true;
+
             View groupDetailView = this.BindingInflate(Resource.Layout.GroupDetailView, null);
 
             return groupDetailView;
+        }
+
+        public override void OnCreateOptionsMenu(IMenu menu, MenuInflater inflater)
+        {
+            base.OnCreateOptionsMenu(menu, inflater);
+
+            bool isEditMode = false;
+            GroupDetailViewModel groupDetailViewModel = (GroupDetailViewModel)ViewModel;
+            if (groupDetailViewModel.SelectedGroup != null)
+            {
+                if (groupDetailViewModel.SelectedGroup.Id != null)
+                {
+                    isEditMode = true;
+                }
+            }
+
+            if (isEditMode)
+            {
+                inflater.Inflate(Resource.Menu.toolbar_menu_edit, menu);
+            }
+            else
+            {
+                inflater.Inflate(Resource.Menu.toolbar_menu_add, menu);
+            }
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.menu_save:
+                    ViewModel.SaveCommand.Execute(null);
+                    return true;
+
+                case Resource.Id.menu_cancel:
+                    ViewModel.CancelCommand.Execute(null);
+                    return true;
+
+                case Resource.Id.menu_delete:
+                    ViewModel.DeleteCommand.Execute(null);
+                    return true;
+
+                default:
+                    return base.OnOptionsItemSelected(item);
+            }
         }
 
         public override void OnViewCreated(View view, Bundle savedInstanceState)
--- Zzz.Droid/Views/PasswordDetailFragment.cs
+++ Zzz.Droid/Views/PasswordDetailFragment.cs
@@ -9,6 +9,7 @@ using Zzz.Core.Models;
 using Zzz.Droid.Activities;
 using Zzz.Droid.Extensions;
 using MvvmCross.Droid.Support.V7.AppCompat.Widget;
+using Android.Widget;
 
 namespace Zzz.Droid.Views
 {
@@ -19,6 +20,9 @@ namespace Zzz.Droid.Views
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             base.OnCreateView(inflater, container, savedInstanceState);
+            // Show the options menu.
+            HasOptionsMenu = true;
+
             View passwordDetailView = this.BindingInflate(Resource.Layout.PasswordDetailView, null);
 
             PasswordDetailViewModel passwordDetailViewModel = (PasswordDetailViewModel)ViewModel;
@@ -48,10 +52,55 @@ namespace Zzz.Droid.Views
 
         }
 
+        public override void OnCreateOptionsMenu(IMenu menu, MenuInflater inflater)
+        {
+            base.OnCreateOptionsMenu(menu, inflater);
+
+            bool isEditMode = false;
+            PasswordDetailViewModel passwordDetailViewModel = (PasswordDetailViewModel)ViewModel;
+            if (passwordDetailViewModel.SelectedPassword != null)
+            {
+                if (passwordDetailViewModel.SelectedPassword.Id != null)
+                {
+                    isEditMode = true;
+                }
+            }
+
+            if (isEditMode)
+            {
+                inflater.Inflate(Resource.Menu.toolbar_menu_edit, menu);
+            }
+            else
+            {
+                inflater.Inflate(Resource.Menu.toolbar_menu_add, menu);
+            }
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.menu_save:
+                    ViewModel.SaveCommand.Execute(null);
+                    return true;
+
+                case Resource.Id.menu_cancel:
+                    ViewModel.CancelCommand.Execute(null);
+                    return true;
+
+                case Resource.Id.menu_delete:
+                    ViewModel.DeleteCommand.Execute(null);
+                    return true;
+
+                default:
+                    return base.OnOptionsItemSelected(item);
+            }
+        }
+
         public override void OnViewCreated(View view, Bundle savedInstanceState)
         {
             base.OnViewCreated(view, savedInstanceState);
-            (this.Activity as MainActivity).SetCustomTitle("Password details");
+            (this.Activity as MainActivity).SetCustomTitle("Password Details");
         }
 
         public override void OnResume()
